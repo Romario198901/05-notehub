19ae029159dc589548dec4bf2ceb677c4f7b6ef5
Roman Hrydin <romario198901@gmail.com>
2026-02-14 22:15:43 +0200

    change Notelist component

--- src/components/Notelist/Notelist.tsx
+++ src/components/Notelist/Notelist.tsx
@@ -1,5 +1,5 @@
 import type { Note } from '../../types/note';
-import css from './Notelist.module.css';
+import css from './NoteList.module.css';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteNote } from '../../services/noteService';
 import toast from 'react-hot-toast';
